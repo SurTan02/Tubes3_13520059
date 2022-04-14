9a83b89853f978d470fe3f1f68e5f3f7ad6328fd
Steven Wen <73114262+loopfree@users.noreply.github.com>
2022-04-14 17:07:47 +0700

    done insert disease to db

--- index.js
+++ index.js
@@ -2,6 +2,61 @@ const path = require("path");
 const express = require("express");
 const history = require("connect-history-api-fallback");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+require("dotenv").config();
+
+/**
+ * this variable will be used to connect to mongoDB
+ * It will be initialized in the .listen function
+ */
+var mongoClient;
+
+/**
+ * wrapper function in order to insert new disease
+ */
+async function insertNewDisease(diseaseName, diseaseGene) {
+	const result = await mongoClient
+		.db("myFirstDatabase")
+		.collection("disease")
+		.insertOne({
+			name: diseaseName,
+			gene: diseaseGene,
+		});
+	return result;
+}
+
+/**
+ * wrapper function in order to
+ * insert new user into db with the following data:
+ * date, name, disease, isinfected, percentage
+ */
+async function insertNewUser(date, name, disease, isInfected, percentage) {
+	const result = await mongoClient
+		.db("myFirstDatabase")
+		.collection("user")
+		.insertOne({
+			date: date,
+			name: name,
+			disease: disease,
+			isInfected: isInfected,
+			percentage: percentage,
+		});
+	return result;
+}
+
+/**
+ * wrapper function in order to get
+ * list disease with the name x.
+ * The function returns array of strings with the disease gene
+ */
+async function getDiseaseGene(name) {
+	const result = await mongoClient
+		.db("myFirstDatabase")
+		.collection("disease")
+		.findOne({ name: name });
+	return result;
+}
 
 const app = express();
 
@@ -44,10 +99,13 @@ let knp = (userGenetic, diseasePrediction) => {
  * sent by the front end and insert the disease
  * into the database
  */
-app.post("/api/insert-disease", function (req, res) {
-	console.log(req.body);
+app.post("/api/insert-disease", async function (req, res) {
+	let result = await insertNewDisease(
+		req.body.diseaseName,
+		req.body.diseaseGene
+	);
 	res.json({
-		message: "wkwk",
+		message: true,
 	});
 });
 
@@ -56,14 +114,23 @@ app.post("/api/insert-disease", function (req, res) {
  * to check if a person has disease or not
  */
 
-app.post("/api/check-disease", function (req, res) {
-	console.log(req.body);
+app.post("/api/check-disease", async function (req, res) {
+	let result = await getDiseaseGene(req.body.diseaseName);
+	if (result) {
+		console.log(result);
+	}
 	res.json({
 		message: knp(req.body.userGene, req.body.diseaseName),
 	});
 });
 
-const port = process.env.SERVER_NAME || 8000;
-app.listen(port, () => {
+const port = process.env.PORT || 8000;
+app.listen(port, async () => {
 	console.log(`Example app listening on port ${port}!`);
+	try {
+		mongoClient = new MongoClient(process.env.MONGO_KEY);
+		await mongoClient.connect();
+	} catch (e) {
+		console.error(e);
+	}
 });
--- src/views/ListDiseaseView.vue
+++ src/views/ListDiseaseView.vue
@@ -1,3 +1,38 @@
+<script>
+import ListDisease from "./../components/ListDisease.vue";
+
+export default {
+	data() {
+		return {
+			receiveInputFunc: undefined,
+		};
+	},
+	components: {
+		ListDisease,
+	},
+	mounted() {
+		/**
+		 * setting up the function to ask data from backend
+		 * only 1 seconds after user has stopped typing
+		 */
+		const debounce = (func, delay) => {
+			let timerId;
+			return function () {
+				clearTimeout(timerId);
+				timerId = setTimeout(() => func.apply(this, arguments), delay);
+			};
+		};
+
+		this.receiveInputFunc = debounce(() => {}, 1000);
+	},
+	methods: {
+		callFunc() {
+			this.receiveInputFunc();
+		},
+	},
+};
+</script>
+
 <template>
 	<div class="container-fluid">
 		<div class="row justify-content-center">
@@ -6,8 +41,18 @@
 					<input
 						type="text"
 						placeholder="<tanggal_prediksi> <nama_penyakit>"
+						@input="callFunc"
 					/>
 				</div>
+			</div>
+		</div>
+		<div
+			class="row justify-content-center mt-5"
+			v-for="index in 10"
+			:key="index"
+		>
+			<div class="col-auto justify-content-center">
+				<ListDisease> Hello </ListDisease>
 			</div>
 		</div>
 	</div>
